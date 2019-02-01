5f15d2c7f3279d48e4bd827b20bbd48ab6796c73
Jimmy Dias <jimmy.dias@gmail.com>
2019-02-01 13:56:35 -0500

    replacing query with post instead of get

--- pypardot/objects/lifecyclestages.py
+++ pypardot/objects/lifecyclestages.py
@@ -12,7 +12,7 @@ class LifecycleStages(object):
         Returns the lifecycle stage matching the specified criteria parameters.
         Supported search criteria: http://developer.pardot.com/kb/api-version-4/lifecycle-stages/#supported-search-criteria
         """
-        response = self._get(path='/do/query', params=kwargs)
+        response = self._post(path='/do/query', params=kwargs)
 
         # Ensure result['lifecycleStage'] is a list, no matter what.
         result = response.get('result')
